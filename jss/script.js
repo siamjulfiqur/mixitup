9df5ad61c4a089d4db1dcbe7bc7c06afe61fb599
siamjulfiqur <142520460+siamjulfiqur@users.noreply.github.com>
2023-11-30 13:03:20 +0600

    Add files via upload

---
+++ jss/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  var mixer = mixitup('#hello', { 
+    animation: {
+        duration: 300
+    }
+});
+
+// light-box
+lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true
+})
+
+});
+
+
+
+
